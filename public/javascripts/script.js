240c78bade1a4eb4447162bd440c76e91a12d58e
Kevin Paek <kpaekn@gmail.com>
2014-03-27 19:15:32 -1000

    added image upload preview. Client side (javascript)

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,22 @@
 (function($) {
 
-  console.log('asdf');
+  // activate tooltips
   $('[data-toggle="tooltip"]').tooltip();
+
+  // image previewer
+  $('.image-input').click(function(e) {
+    e.preventDefault();
+    var img = $(this).find('img');
+    var target = $(this).attr('data-target');
+    $(target).one('change', function(e) {
+      if(this.files && this.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+          img.attr('src', e.target.result);
+        }
+        reader.readAsDataURL(this.files[0]);
+      }
+    }).click();
+  });
 
 })(jQuery);
