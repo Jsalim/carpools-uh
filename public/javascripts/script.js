0ae5471be805c651416109bb62234c6960e9cc07
Kevin Paek <kpaekn@gmail.com>
2014-03-30 12:51:26 -1000

    added javascript to handle the hidden inputs so that validation works correctly for the images

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,28 +7,34 @@
   $('.image-previewer').each(function(i, el) {
     var link = $(el).find('.image-input');
     var img = $(el).find('img');
-    var input = $(el).find('input[type="file"]');
+    var fileInput = $(el).find('input[type="file"]');
+    var hiddenInput = $(el).find('input[type="hidden"]');
     var removeImage = $(el).find('.remove-image');
+
     link.click(function(e) {
       e.preventDefault();
-      input.one('change', function() {
+      fileInput.one('change', function() {
         if(this.files && this.files[0]) {
           var reader = new FileReader();
           reader.onload = function(e) {
             img.attr('src', e.target.result);
+            hiddenInput.val("exists");
             removeImage.prop('checked', false);
           }
           reader.readAsDataURL(this.files[0]);
         }
       }).click();
     });
+
     removeImage.change(function(e) {
       var checked = $(this).prop('checked');
       if(checked) {
-        img.attr('data-save', img.attr('src'));
+        img.data('save', img.attr('src'));
         img.attr('src', img.attr('data-default'));
+        hiddenInput.val("");
       } else {
-        img.attr('src', img.attr('data-save'));
+        img.attr('src', img.data('save'));
+        hiddenInput.val("exists");
       }
     });
   });
